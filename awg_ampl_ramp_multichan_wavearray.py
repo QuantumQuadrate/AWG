322d959ae02c578fcb0f3bd407cde07ccfe89304
prhuft <gothrowayo@gmail.com>
2021-09-01 10:17:03 -0500

    two channel Gaussian output with artificial pulse delay

--- awg_ampl_ramp_multichan_wavearray.py
+++ awg_ampl_ramp_multichan_wavearray.py
@@ -3,15 +3,19 @@ Multiple channel, single waveform per channel, amplitude ramp from an array.
 
 P. Huft
 
-Connect a 3.3 V trigger to the AWG card and run this script to 
-generate a Gaussian pulse with a channel dependent delay on each
-channel once per trigger. 
+Connect a 3.3 V trigger to the AWG card and run this script to generate one
+Gaussian pulse on each channel per trigger. Note that I could not get the 
+startDelay argument in AWGqueueWaveform to have any effect. Nevertheless,
+a pulse delay can be acheived by simply adding an offset in the pulse function,
+e.g. changing the mean of one of the Gaussian pulses.
 """
 
 import os
 import sys;
 import numpy as np
 from collections import namedtuple
+from matplotlib import pyplot as plt
+
 sys.path.insert(0,'C:/Program Files/Signadyne/Libraries/Python')
 import signadyne as sd
 
@@ -36,13 +40,19 @@ ModTypes = {'amplitude':'modulationAmplitudeConfig', 'frequency':'modulationAngl
 wfm_mod = 'amplitude'
 wfm_tau = 5e-7 # duration
 wfm_pts = 100
+dx = 20
 sigma = wfm_pts/10 # make this small so endpts of the wave are near 0
-wfm_arr = np.array([np.exp(-x**2/(2*sigma**2)) for x in np.linspace(-wfm_pts/2,wfm_pts/2,wfm_pts+1)])
-wfm_arr[0] = wfm_arr[-1] = 0 # set the endpts to zero
+xpts = np.linspace(-wfm_pts/2,wfm_pts/2,wfm_pts+1)
+wfm_arr0 = np.array([np.exp(-x**2/(2*sigma**2)) for x in xpts])
+# wfm_arr1 = np.linspace(0,1,wfm_pts+1) #
+wfm_arr1 = np.array([np.exp(-(x-dx)**2/(2*sigma**2)) for x in xpts])
 
 # duration of the waveform to prescaler factor
 prescl_from_tau = lambda tau: int(tau*f_clk/(5*wfm_pts)) # manual: 1.2.2.4 Prescaler and Sampling Rate
 prescl = prescl_from_tau(wfm_tau)
+
+f_sample = f_clk/(5*prescl)
+print("sample rate:", f_sample/1e6, "MHz")
 
 # when to start after the trigger
 delay = 10 # [10 ns]
@@ -50,10 +60,11 @@ delay = 10 # [10 ns]
 # mod_type attr is unused in this script; amplitude modulation harcoded.
 mod_type = ModTypes['amplitude']
 start_trig = sd.SD_TriggerModes.EXTTRIG_CYCLE
-wfm0 = Waveform(mod_type,wfm_tau,wfm_pts,sigma,wfm_arr,prescl,delay,start_trig,sd.SD_Wave())
-wfm1 = Waveform(mod_type,wfm_tau,wfm_pts,sigma,wfm_arr,prescl,0,start_trig,sd.SD_Wave())
-wfm1 = Waveform(mod_type,wfm_tau,wfm_pts,sigma,wfm_arr,prescl,delay,start_trig,sd.SD_Wave())
+wfm0 = Waveform(mod_type,wfm_tau,wfm_pts,sigma,wfm_arr0,prescl,0,start_trig,sd.SD_Wave())
+wfm1 = Waveform(mod_type,wfm_tau,wfm_pts,sigma,wfm_arr1,prescl,delay,start_trig,sd.SD_Wave())
 waveforms = [wfm0, wfm1]
+# for wv in waveforms:
+    # wv.arr[0] = wv.arr[-1] = 0 # set the endpts to zero
 
 """channel defaults"""
 
@@ -87,13 +98,14 @@ else:
             )        
         
         # load the wave data from a 1D array of floats
+        # TODO: instantiate and load SDwaves outside the card/chan setup
         error = wfm.SDwave.newFromArrayDouble(
             waveformType=sd.SD_WaveformTypes.WAVE_ANALOG, 
             waveformDataA=wfm.arr
             )
         
         # load the wave into the AWG RAM
-        error = card.waveformLoad(wfm.SDwave, chan)
+        error = card.waveformLoad(wfm.SDwave, chan) # should be wfm number, not chan number
         
         # modulation - amplitude.
         error = card.modulationAmplitudeConfig(
@@ -123,7 +135,7 @@ else:
         # queue waveform from RAM
         card.AWGqueueWaveform(
             nAWG=chan,
-            waveformNumber=0,
+            waveformNumber=chan, # the number of waveform in the RAM
             triggerMode=wfm.trig_type, # see Manual 1.2.2.5 Trigger
             startDelay=wfm.delay,
             cycles=0, # 0: infinite cycles
@@ -142,3 +154,17 @@ else:
         
     card.close()
     print("AOU closed")
+    
+    
+    fig, ax = plt.subplots()
+    ax.set_title('Modulation envelope waveforms')
+    ax.set_xlabel('real output time [$\mu$s]')
+    ax.set_ylabel('amplitude [arb.]')
+
+    colors = ['yellow','red','cyan','green']
+    for i,wfm in enumerate(waveforms):
+        ax.plot(xpts*(1/f_sample)*1e6, wfm.arr, label=i,color=colors[i])
+    ax.legend()
+        
+    plt.show()
+        
